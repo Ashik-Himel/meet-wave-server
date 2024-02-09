02f38d81ef66bd3ac6a054c352c73f4b8371ac7f
Ashik-Himel <ashikujjamanhimel@gmail.com>
2024-02-09 20:17:28 +0600

    users tracking added for dashboard

--- index.js
+++ index.js
@@ -23,10 +23,55 @@ async function run() {
   try {
     const database = client.db('MeetWave');
     const meetingLinks = database.collection('meeting-links');
+    const allUsersCollection = database.collection('all-users');
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("MongoDB Connected!!!");
+
+
+    // users related API
+    app.post('/all-users', async (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const query = { email: user?.email };
+      const existingUser = await allUsersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exits", insertedId: null })
+      };
+      const result = await allUsersCollection.insertOne(user);
+      console.log(result);
+      res.send(result)
+    })
+
+    // get all users
+    app.get('/all-users', async (req, res) => {
+      const result = await allUsersCollection.find().toArray();
+      res.send(result)
+    })
+
+    // get single user
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await allUsersCollection.findOne(query);
+      res.send(result);
+    })
+
+    // Update an User status
+    app.patch('/user-status/:email', async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const user = await allUsersCollection.findOne({ email: email });
+      const updatedStatus = !user.status;
+      const result = await allUsersCollection.updateOne(
+        { email: email },
+        { $set: { status: updatedStatus } }
+      );
+      res.send(result);
+    });
+
+
   } finally {
     // await client.close();
   }
